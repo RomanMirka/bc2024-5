dc8a3d134595634e4cd1c2ce0d1c4f2151307ffc
RomanMirka bc2024-5 <romamirkastudy@gmail.com>
2024-11-10 16:55:40 +0200

    small fixes

---
+++ main.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { program } = require('commander');
+const fs = require('fs');
+
+program
+  .requiredOption('-h, --host <host>', 'server address')
+  .requiredOption('-p, --port <port>', 'server port')
+  .requiredOption('-c, --cache <cache>', 'directory path');
+
+program.parse(process.argv);
+
+const o = program.opts();
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('Server`s alive');
+});
+
+// Запуск сервера
+app.listen(o.port, o.host, () => {
+  console.log(`Server running at http://${o.host}:${o.port}`);
+});
